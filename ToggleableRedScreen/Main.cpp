5d2852af1fc79910b3c745b7a88609a173f6c792
Dreamy Cecil <stalker27production@gmail.com>
2022-07-11 16:00:45 +0300

    Reimplement toggleable red screen on damage as a plugin for the game.

---
+++ ToggleableRedScreen/Main.cpp
@@ -0,0 +1,89 @@
+/* Copyright (c) 2022 Dreamy Cecil
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#include "StdH.h"
+
+#include <CoreLib/World/WorldFunctions.h>
+
+// Patcher
+#include <CoreLib/FuncPatching.h>
+
+bool CPatch::_bDebugOutput = false;
+
+// Define own pointer to the API
+CCoreAPI *_pCoreAPI = NULL;
+
+// Retrieve module information
+MODULE_API void Module_GetInfo(CPluginAPI::PluginInfo *pInfo) {
+  // Used API and utility
+  pInfo->apiVer = CORE_API_VERSION;
+  pInfo->ulFlags = CPluginAPI::PF_GAME | CPluginAPI::PF_EDITOR;
+
+  // Metadata
+  pInfo->strAuthor = "Dreamy Cecil";
+  pInfo->strName = "Toggleable Red Screen";
+  pInfo->strDescription = "A new console command that allows toggling of red screen on damage.";
+  pInfo->ulVersion = CCoreAPI::MakeVersion(1, 0, 0);
+};
+
+// Toggle red screen on damage
+static INDEX sam_bRedScreenOnDamage = TRUE;
+
+// Original function pointer
+static void (CDrawPort::*pBlendScreen)(void) = NULL;
+
+// Patched function
+class CDrawPortPatch : public CDrawPort {
+  public:
+    void P_BlendScreen(void)
+    {
+      if (!sam_bRedScreenOnDamage) {
+        BOOL bReset = FALSE;
+
+        // Reset blending altogether if no world glaring available
+        if (IWorld::GetWSC(IWorld::GetWorld()) == NULL) {
+          bReset = TRUE;
+
+        // Reset red screen blending
+        } else if (dp_ulBlendingRA >  0 && dp_ulBlendingA  >  0
+                && dp_ulBlendingGA == 0 && dp_ulBlendingBA == 0) {
+          bReset = TRUE;
+        }
+
+        if (bReset) {
+          dp_ulBlendingRA = 0;
+          dp_ulBlendingGA = 0;
+          dp_ulBlendingBA = 0;
+          dp_ulBlendingA  = 0;
+        }
+      }
+
+      // Proceed to the original function
+      (this->*pBlendScreen)();
+    };
+};
+
+// Module entry point
+MODULE_API void Module_Startup(void) {
+  // Hook pointer to the API
+  HookSymbolAPI();
+
+  // Patch screen blending function
+  pBlendScreen = &CDrawPort::BlendScreen;
+  NewPatch(pBlendScreen, &CDrawPortPatch::P_BlendScreen, "CDrawPort::BlendScreen()");
+
+  // Custom symbols
+  _pShell->DeclareSymbol("persistent user INDEX sam_bRedScreenOnDamage;", &sam_bRedScreenOnDamage);
+};
