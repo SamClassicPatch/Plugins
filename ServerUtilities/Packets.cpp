728a26577257753ea3ba3dcc6a8405329e6c6460
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-01-21 18:05:50 +0300

    Prevent cheating when 3rd person view is disabled in ServerUtilities.

--- ServerUtilities/Packets.cpp
+++ ServerUtilities/Packets.cpp
@@ -15,26 +15,32 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "StdH.h"
 
-// Define packet events for the plugin
+#include <CoreLib/Networking/Modules/ActiveClients.h>
+
+// Handle third person option on character change
+static void HandleThirdPerson(INDEX iClient, CPlayerCharacter &pc)
+{
+  // Disallow third person view
+  if (_psThirdPerson.GetIndex() == -1) return;
+
+  CPlayerSettings *pps = (CPlayerSettings *)pc.pc_aubAppearance;
+
+  // Check if client keeps trying to change the option
+  if (pps->ps_ulFlags & PSF_PREFER3RDPERSON) {
+    _aActiveClients[iClient].ctAnnoyanceLevel += 10;
+  }
+
+  pps->ps_ulFlags &= ~PSF_PREFER3RDPERSON;
+};
 
 void IPacketEvents::OnCharacterConnect(INDEX iClient, CPlayerCharacter &pc)
 {
-  CPlayerSettings *pps = (CPlayerSettings *)pc.pc_aubAppearance;
-
-  // Disallow third person view
-  if (_psThirdPerson.GetIndex() != -1) {
-    pps->ps_ulFlags &= ~PSF_PREFER3RDPERSON;
-  }
+  HandleThirdPerson(iClient, pc);
 };
 
 BOOL IPacketEvents::OnCharacterChange(INDEX iClient, INDEX iPlayer, CPlayerCharacter &pc)
 {
-  CPlayerSettings *pps = (CPlayerSettings *)pc.pc_aubAppearance;
-
-  // Disallow third person view
-  if (_psThirdPerson.GetIndex() != -1) {
-    pps->ps_ulFlags &= ~PSF_PREFER3RDPERSON;
-  }
+  HandleThirdPerson(iClient, pc);
   return TRUE;
 };
 
@@ -47,7 +53,14 @@ void IPacketEvents::OnPlayerAction(INDEX iClient, INDEX iPlayer, CPlayerAction &
   INDEX i3rdPersonButton = _psThirdPerson.GetIndex();
 
   if (i3rdPersonButton != -1) {
-    pa.pa_ulButtons &= ~(1 << i3rdPersonButton);
+    const INDEX iBit = (1 << i3rdPersonButton);
+
+    // Check if client keeps pressing the button
+    if (iResent == -1 && pa.pa_ulButtons & iBit) {
+      _aActiveClients[iClient].ctAnnoyanceLevel += 10;
+    }
+
+    pa.pa_ulButtons &= ~iBit;
   }
 };
 
